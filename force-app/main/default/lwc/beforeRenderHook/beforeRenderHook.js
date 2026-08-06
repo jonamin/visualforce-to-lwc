b2e58670d27ecab2152ad515b55556cd32f66b49
Serpent Compare Org and Deploy <deployment@serpent.local>
2026-08-06 12:59:55 +0300

    chore: sync target org metadata

--- force-app/main/default/lwc/beforeRenderHook/beforeRenderHook.js
+++ force-app/main/default/lwc/beforeRenderHook/beforeRenderHook.js
@@ -2,7 +2,6 @@ import { LightningElement } from 'lwc';
 
 export default class BeforeRenderHook extends LightningElement {
     courses;
-    //hello
 
     connectedCallback() {
         this.courses = ['Irrigation Systems', 'Soils', 'Organic Crops'];
